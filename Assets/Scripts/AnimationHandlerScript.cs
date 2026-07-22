adb0db7e702d99a2cab89562908e4f7a0faa3590
Oliver-Plein <oplein39@gmail.com>
2026-07-22 18:12:43 -0500

    Raccoon Anims

--- Assets/Scripts/AnimationHandlerScript.cs
+++ Assets/Scripts/AnimationHandlerScript.cs
@@ -7,12 +7,18 @@ public class AnimationHandlerScript : MonoBehaviour
     public Sprite JimothyHumanFall;
     public Sprite JimothyHumanRun1;
     public Sprite JimothyHumanRun2;
+    public Sprite JimothyRaccoonFall;
+    public Sprite JimothyRaccoonCrouch;
+    public Sprite JimothyRaccoonRun1;
+    public Sprite JimothyRaccoonRun2;
+    public Sprite JimothyRaccoonCrouch2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Application.targetFrameRate = 60;
     }
     //true means human, false is raccoon, bool quicker than string lmao
+    public bool JimothyCrouch = false;
     public bool JimothyForm = true;
     public int AnimationFrameCount = 0;
     public int RunAnimationSpeed = 20;
@@ -35,6 +41,39 @@ public class AnimationHandlerScript : MonoBehaviour
                 if (AnimationFrameCount == RunAnimationSpeed)
                 {
                     Jimothy.GetComponent<SpriteRenderer>().sprite= JimothyHumanRun2;
+                    AnimationFrameCount = 0;
+                }
+            }
+        }
+        if (JimothyForm == false)
+        {
+            if (Jimothy.GetComponent<PlayerScript>().OnGround == false)
+            {
+                Jimothy.GetComponent<SpriteRenderer>().sprite = JimothyRaccoonFall;
+            }
+            else if (JimothyCrouch == true)
+            {
+                AnimationFrameCount++;
+                if (AnimationFrameCount == (RunAnimationSpeed / 2))
+                {
+                    Jimothy.GetComponent<SpriteRenderer>().sprite = JimothyRaccoonCrouch;
+                }
+                if (AnimationFrameCount == RunAnimationSpeed)
+                {
+                    Jimothy.GetComponent<SpriteRenderer>().sprite = JimothyRaccoonCrouch2;
+                    AnimationFrameCount = 0;
+                }
+            }
+            else
+            {
+                AnimationFrameCount++;
+                if (AnimationFrameCount == (RunAnimationSpeed / 2))
+                {
+                    Jimothy.GetComponent<SpriteRenderer>().sprite = JimothyRaccoonRun1;
+                }
+                if (AnimationFrameCount == RunAnimationSpeed)
+                {
+                    Jimothy.GetComponent<SpriteRenderer>().sprite = JimothyRaccoonRun2;
                     AnimationFrameCount = 0;
                 }
             }
