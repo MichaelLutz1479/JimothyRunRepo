6d0d0e97935e27edc557a9f91b73157c25f1b5df
Oliver-Plein <oplein39@gmail.com>
2026-07-22 17:35:45 -0500

    Update PlayerScript.cs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,10 +1,59 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //ADD THIS TO THE SCRIPT OTHERWISE INPUT ACTION WILL NOT WORK
+
+
+public class TransformScript : MonoBehaviour
+{
+    public bool onGround = false;
+
+    bool transformed = false;
+
+
+    /*
+     Do not put the input actions in Update()
+     I can set up the inspector stuff and keybinds, but if you try to run the game before I add those you will get errors, just ignore them.
+     */
+
+    //Z, F
+    public void TransfromAction(InputAction.CallbackContext context)
+    {
+        if (transformed == false)
+        {
+            transformed = true;
+        }
+        else if (transformed == true)
+        {
+            transformed = false;
+        }
+    }
+
+    //W, Up arrow, Spacebar
+    public void JumpAction(InputAction.CallbackContext context)
+    {
+        if (transformed == false && onGround == true)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 350f));
+        }
+    }
+
+    //S, Down Arrow
+    public void CrouchAction(InputAction.CallbackContext context)
+    {
+        if (transformed == true)
+        {
+
+        }
+    }
+
+}
+
+void Start()
     {
         GetComponent<Rigidbody2D>();
         OnGround = true;
