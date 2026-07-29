316e38a1fa1dff35fc413754a2fe6c3e50f49352
MichaelLutz1479 <michaellutz1479@gmail.com>
2026-07-29 15:52:58 -0700

    Fixed looped songs

--- Assets/Scripts/Michael's temp scripts/SoundHandler.cs
+++ Assets/Scripts/Michael's temp scripts/SoundHandler.cs
@@ -1,12 +1,23 @@
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundHandler : MonoBehaviour
 {
-    public AudioSource mainLoop;
+    public AudioSource mainMenuLoop;
+    
+
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "JimothyRun") 
+        {
+            mainMenuLoop.Stop();
+        }
+    }
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
-        mainLoop.Play();
+        mainMenuLoop.Play();
     }
 }
