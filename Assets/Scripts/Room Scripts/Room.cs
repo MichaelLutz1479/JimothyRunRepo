5adcc3a1d179fde7a76e1e02f59b1dbac3abe0d4
MichaelLutz1479 <michaellutz1479@gmail.com>
2026-07-28 17:01:15 -0700

    I added room spawning logic

--- Assets/Scripts/Room Scripts/Room.cs
+++ Assets/Scripts/Room Scripts/Room.cs
@@ -10,14 +10,14 @@ public class Room : MonoBehaviour
 
     private RoomHandler handler;
 
+
+
     public void SetHandler(RoomHandler roomHandler)
     {
-        handler = roomHandler;
-    }
-
-
-    void Start()
-    {
+            Debug.Log("========== SET HANDLER HIT =========");
+            handler = roomHandler;
+            Debug.Log(gameObject.name + " assigned handler AFTER SET: " + handler);
+            Debug.Log(gameObject.name + " receieved handler");
 
     }
 
@@ -28,12 +28,25 @@ public class Room : MonoBehaviour
 
     void Update()
     {
+        Debug.Log("========== UPDATE HIT =========");
+
         transform.position = new Vector3(transform.position.x - roomSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+
+        Debug.Log(gameObject.name + " assigned handler UPDATE: " + handler);
 
         if (!hasSpawnedNext && transform.position.x < 20f)
         {
+            if (handler != null)
+            {
             handler.SpawnRoom();
+
+            }
+            else
+            {
+                Debug.LogError(gameObject.name + " has no room handler");
+            }
             hasSpawnedNext = true;
+
         }
 
         if(transform.position.x < destroyX)
--- Assets/Scripts/Room Scripts/RoomHandler.cs
+++ Assets/Scripts/Room Scripts/RoomHandler.cs
@@ -5,13 +5,15 @@ public class RoomHandler : MonoBehaviour
 {
     public List<GameObject> roomPrefabs;
 
-    /*public List<GameObject> startHigh;
+    public List<GameObject> startHigh;
     public List<GameObject> startMid;
     public List<GameObject> startLow;
 
     public List<GameObject> endHigh;
     public List<GameObject> endMid;
-    public List<GameObject> endLow;*/
+    public List<GameObject> endLow;
+
+    private GameObject previousRoom;
 
     public float roomSpeed = 5f;
     public float roomWidth = 10f;
@@ -19,6 +21,9 @@ public class RoomHandler : MonoBehaviour
     private Room lastRoom;
 
     float nextSpawnX = 14f;
+
+
+
 
 
     void Start()
@@ -36,9 +41,47 @@ public class RoomHandler : MonoBehaviour
 
     public void SpawnRoom()
     {
+        if (endHigh.Contains(previousRoom))
+        {
+            Debug.Log("Previous Room: " + previousRoom);
+        }
+
+        Debug.Log("RoomPrefabs: " + roomPrefabs.Count + " High: " + startHigh.Count + " Mid: " + startMid.Count + " Low: " + startLow.Count + " High: " + endHigh.Count + " Mid: " + endMid.Count + " Low: " +endLow.Count);
+
+        Debug.Log("Is previous room in endHigh: " + endHigh.Contains(previousRoom));
+        Debug.Log("Is previous room in endMid: " + endMid.Contains(previousRoom));
+        Debug.Log("Is previous room in endLow: " + endLow.Contains(previousRoom));
+
         GameObject chosenRoom = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+        if (chosenRoom == null)
+        {
+            Debug.LogError("No valid room found for this connection");
+            return;
+        }
+
+        if (lastRoom == null)
+        {
+            chosenRoom = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+        }
+        else
+        {
+            if (endHigh.Contains(previousRoom))
+            {
+                chosenRoom = startHigh[Random.Range(0, startHigh.Count)];
+            }
+            else if (endMid.Contains(previousRoom))
+            {
+                chosenRoom = startMid[Random.Range(0, startMid.Count)];
+            }
+            else if (endLow.Contains(previousRoom))
+            {
+                chosenRoom = startLow[Random.Range(0, startLow.Count)];
+            }
+        }
 
         GameObject newRoom;
+
+        Debug.Log("Spawned: " + chosenRoom.name);
 
         if(lastRoom == null)
         {
@@ -46,6 +89,7 @@ public class RoomHandler : MonoBehaviour
         }
         else
         {
+
             newRoom = Instantiate(chosenRoom, new Vector3(nextSpawnX, 0, 1), Quaternion.identity);
         }
 
@@ -53,8 +97,15 @@ public class RoomHandler : MonoBehaviour
 
         Room room = newRoom.GetComponent<Room>();
 
+        Debug.Log("Got Room component: " + room);
+
         room.SetSpeed(roomSpeed);
         room.SetHandler(this);
+
+        Debug.Log("Handler assigned to: " + newRoom.name);
+
+        previousRoom = chosenRoom;
+
         lastRoom = room;
 
         nextSpawnX += roomWidth;
